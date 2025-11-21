b33d40cd28c3f9d736f016d5157536aee69dc9fb
anbeatrizduarte <bana2229@gmail.com>
2025-11-21 18:06:19 -0300

    Organização do fluxo do projeto

--- js/componentes/ui/Section.js
+++
@@ -1,7 +0,0 @@
-function Section() {
-    return (
-        <div className="border-4 border-solid border-red-600 w-[150%] h-64 m-24 ml-32">
-            <Text as="span" variant="titulo">Minhas Tarefas</Text>
-        </div>
-    )
-}
