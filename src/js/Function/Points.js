1df4c38cb0f5a81bd9cbe51dae1f4f554cc298de
anbeatrizduarte <bana2229@gmail.com>
2025-11-28 22:52:16 -0300

    feat(telaPontos): Criação da funcionalide de adicionar ocorrências na tabela de pontuação

---
+++ src/js/Function/Points.js
@@ -0,0 +1,36 @@
+const groupPet = {
+    petiano1: { namePetiano: "Ana Beatriz de Oliveira Duarte", pontuacao: 10 },
+    petiano2: { namePetiano: "Camila Pinheiro Dias", pontuacao: 10 },
+    petiano3: { namePetiano: "Gabriel Marques Araujo", pontuacao: 10 }
+}
+
+function addOccurrence(resIptNamePetiano, resIptPointPetiano, resIptDescPetiano, setOccurrences) {
+    console.log(resIptNamePetiano)
+    console.log(resIptPointPetiano)
+    console.log(resIptDescPetiano)
+
+    const points = Number(resIptPointPetiano);
+
+
+    Object.values(groupPet).forEach((petiano) => {
+        if (petiano.namePetiano === resIptNamePetiano) {
+
+            if (points < 0 || petiano.pontuacao < 10) {
+                petiano.pontuacao += points;
+                if (petiano.pontuacao > 10) {
+                    petiano.pontuacao = 10;
+                }
+            }
+        }
+    });
+
+    const occurrence = { resIptNamePetiano, resIptPointPetiano, resIptDescPetiano }
+    setOccurrences(prev => [...prev, { resIptNamePetiano, points, resIptDescPetiano }]);
+}
+
+window.addOccurrence = addOccurrence;
+
+
+
+
+
