4d5969a24bbe45c8d13ce909e114cc78053fffbc
anbeatrizduarte <bana2229@gmail.com>
2025-12-04 13:21:50 -0300

    fix(main): correções dos códigos para deploy do projeto no GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './ui/Layout';
-import { Home } from './pages/Home';
-import { Perfil } from './pages/Perfil';
-import { Pontos } from './pages/Pontos';
-import { Modelo } from './pages/Modelo';
-
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from './ui/Layout'
+import { Home } from './pages/Home'
+import { Perfil } from './pages/Perfil'
+import { Pontos } from './pages/Pontos'
+import { Modelo } from './pages/Modelo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
 
         <Route path="/" element={<Layout />}>
@@ -27,7 +20,7 @@ function App() {
 
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
